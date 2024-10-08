bddedce35b4a1f59f7c2c16e85f3132743c4fde6
mhadev0703 <minchulha0703@gmail.com>
2024-10-07 23:20:33 -0400

    Add lambda function handler

--- packages/functions/src/lambda.ts
+++ packages/functions/src/lambda.ts
@@ -1,8 +1,69 @@
-import { ApiHandler } from "sst/node/api";
+import { APIGatewayProxyEventV2 } from 'aws-lambda';
 
-export const handler = ApiHandler(async (_evt) => {
-  return {
-    statusCode: 200,
-    body: `Hello world. The time is ${new Date().toISOString()}`,
-  };
-});
+const BASE_PRICE = 3900;
+
+export const handler = async (event: APIGatewayProxyEventV2) => {
+    if (event.requestContext.http.method === 'GET') {
+        if (event.rawPath === '/collections') {
+            return {
+                statusCode: 200,
+                body: JSON.stringify({}),
+            };
+        } else if (event.rawPath === '/getImages') {
+            return {
+                statusCode: 200,
+                body: JSON.stringify({}),
+            };
+        } else {
+            return {
+                statusCode: 404,
+                body: JSON.stringify({
+                    error: true,
+                    message: 'Hello world. This is a bad request.',
+                }),
+            };
+        }
+    } else if (event.requestContext.http.method === 'POST') {
+        if (event.rawPath === '/updateCollectionStatus') {
+            return {
+                statusCode: 200,
+                body: JSON.stringify({}),
+            };
+        } else if (event.rawPath === '/createCollection') {
+            return {
+                statusCode: 200,
+                body: JSON.stringify({}),
+            };
+        } else if (event.rawPath === '/payment') {
+            return {
+                statusCode: 200,
+                body: JSON.stringify({}),
+            };
+        } else if (event.rawPath === '/execBanana') {
+            return {
+                statusCode: 200,
+                body: JSON.stringify({}),
+            };
+        } else if (event.rawPath === '/checkStartDatetime') {
+            return {
+                statusCode: 200,
+                body: JSON.stringify({}),
+            };
+        } else if (event.rawPath === '/cancelPayment') {
+            return {
+                statusCode: 200,
+                body: JSON.stringify({}),
+            };
+        } else {
+            return {
+                statusCode: 404,
+                body: 'Hello world. This is a bad request.',
+            };
+        }
+    } else {
+        return {
+            statusCode: 400,
+            body: 'Hello world. This is a bad request.',
+        };
+    }
+};
