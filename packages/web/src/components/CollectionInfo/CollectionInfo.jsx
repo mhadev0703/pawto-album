bb4b34b908efa682b927f0dfc202cf63dcf1245e
mhadev0703 <minchulha0703@gmail.com>
2024-10-23 23:59:28 -0400

    Add email handler with input

--- packages/web/src/components/CollectionInfo/CollectionInfo.jsx
+++ packages/web/src/components/CollectionInfo/CollectionInfo.jsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 import './CollectionInfo.css';
 
@@ -5,18 +6,92 @@ export default function CollectionInfo(props) {
     const croppedImages = props.croppedImages;
     const changeCroppingImageIndex = props.changeCroppingImageIndex;
 
+    const [title, setTitle] = useState('');
+    const [email, setEmail] = useState('');
+    const [emailError, setEmailError] = useState('');
+
+    const isValidEmail = (email) => {
+        return /\S+@\S+\.\S+/.test(email);
+    };
+
+    const changeEmail = (e) => {
+        if (!isValidEmail(e.target.value)) {
+            setEmailError('Please enter a valid email address.');
+        } else {
+            setEmailError('');
+        }
+
+        setEmail(e.target.value);
+    };
+
+    const changeTitle = (e) => {
+        setTitle(e.target.value);
+    };
+
     return (
-        <div className='upload-image-preview'>
-            {croppedImages.map((file, index) => {
-                return (
-                    <img
-                        onClick={() => changeCroppingImageIndex(index)}
-                        key={index}
-                        className='preview-image'
-                        src={file}
-                    />
-                );
-            })}
+        <div className='collection-info-container'>
+            <div className='input-field'>
+                <div className='input-info'>
+                    <label>Title</label>
+                </div>
+                <input
+                    className='custom-input-text'
+                    type='text'
+                    value={title}
+                    onChange={(e) => {
+                        changeTitle(e);
+                    }}
+                    placeholder='e.g., Our dog Poppy, max 20 characters'
+                    maxLength={20}
+                />
+            </div>
+
+            <div className='input-field'>
+                <div className='input-info'>
+                    <label>Email</label>
+                </div>
+
+                <input
+                    className='custom-input-text'
+                    type='email'
+                    value={email}
+                    onChange={(e) => {
+                        changeEmail(e);
+                    }}
+                    placeholder='All progress updates will be sent to your email'
+                />
+                <div
+                    style={{
+                        height: emailError !== '' ? '20px' : '0px',
+                        fontWeight: 'bold',
+                        color: '#F24646',
+                        margin: '3px 8px',
+                        fontSize: '12px',
+                    }}
+                >
+                    {emailError}
+                </div>
+            </div>
+            <div className='input-field'>
+                <div className='input-info'>
+                    <label>Photos</label>
+                    <span>
+                        - Click on the photo to adjust the cropping position around the subject
+                    </span>
+                </div>
+                <div className='upload-image-preview'>
+                    {croppedImages.map((file, index) => {
+                        return (
+                            <img
+                                onClick={() => changeCroppingImageIndex(index)}
+                                key={index}
+                                className='preview-image'
+                                src={file}
+                            />
+                        );
+                    })}
+                </div>
+            </div>
         </div>
     );
 }
