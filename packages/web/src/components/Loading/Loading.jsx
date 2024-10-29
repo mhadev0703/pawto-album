fa5671dd2cc1dcaf2a5fcee79ab8d2b152d2ca66
mhadev0703 <minchulha0703@gmail.com>
2024-10-28 23:59:24 -0400

    Add loading component

---
+++ packages/web/src/components/Loading/Loading.jsx
@@ -0,0 +1,11 @@
+import './Loading.css';
+
+export default function Loading() {
+    return (
+        <div className='bouncing-loader'>
+            <div></div>
+            <div></div>
+            <div></div>
+        </div>
+    );
+}
