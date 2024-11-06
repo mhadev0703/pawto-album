cd7e014e8cec5af24000cae44ab6891bb9ef3534
mhadev0703 <minchulha0703@gmail.com>
2024-11-05 23:59:31 -0500

    Add skeleton code

--- packages/web/src/pages/Order/Order.jsx
+++ packages/web/src/pages/Order/Order.jsx
@@ -1,3 +1,36 @@
-export default function Order() { 
-  return <div>Order</div>; 
+import Loading from '../../components/Loading/Loading';
+import Header from '../../components/Header/Header';
+
+export default function Order() {
+  return (
+    <div>
+      {isLoading ? (
+        <Loading />
+      ) : (
+        <div className='App'>
+          <div>
+            <Header
+              subHeading={
+                'Please proceed with the payment to view the generated results.'
+              }
+            />
+          </div>
+          <div className='container'>
+            {paidStatus ? (
+              <div
+                style={{ fontWeight: '500', fontSize: '20px' }}
+              >
+                Your payment has been completed! <br />
+                Please check the link sent to your email to track the status of your order.
+              </div>
+            ) : (
+              <div className='center-button'>
+                <button>CHECKOUT</button>
+              </div>
+            )}
+          </div>
+        </div>
+      )}
+    </div>
+  );
 }
