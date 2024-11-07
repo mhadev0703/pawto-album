77e1ec207d965adcb9a1afec6453462776e0d2d0
mhadev0703 <minchulha0703@gmail.com>
2024-11-07 00:00:10 -0500

    Implement order component

--- packages/web/src/pages/Order/Order.jsx
+++ packages/web/src/pages/Order/Order.jsx
@@ -1,7 +1,72 @@
+import { useEffect, useState } from 'react';
+import { useSearchParams } from 'react-router-dom';
+import axios from 'axios';
 import Loading from '../../components/Loading/Loading';
 import Header from '../../components/Header/Header';
 
+const REACT_APP_API_ADDRESS = import.meta.env.VITE_API_URL;
+const REACT_APP_ADDRESS = import.meta.env.VITE_APP_URL;
+
 export default function Order() {
+
+  // Get the collectionId from the URL
+  const [searchParams, setSearchParams] = useSearchParams();
+  const collectionId = searchParams.get('collectionId');
+
+  const [isLoading, setIsLoading] = useState(false);
+  const [collectionData, setCollectionData] = useState({
+    collectionStatus: '',
+    createDatetime: '',
+    email: '',
+    endDatetime: '',
+    animalType: '',
+    name: '',
+    paid: false,
+    price: '',
+    receipt: '',
+    startDatetime: '',
+  });
+  const [paidStatus, setPaidStatus] = useState(false);
+
+  useEffect(() => {
+    if (collectionId !== null) {
+      checkPaidStatus();
+    }
+  }, []);  // Run only once
+
+  const checkPaidStatus = async () => {
+    setIsLoading(true);
+
+    let url = `${REACT_APP_API_ADDRESS}/collections?collectionId=${collectionId}`;
+
+    axios
+      .get(url)
+      .then((res) => {
+        if (res.data) {
+          console.log(res.data);
+          if (res.data.error) {
+            alert(res.data.error);
+            return;
+          } else {
+            setCollectionData(res.data);
+            
+            let collectionStatus = parseInt(res.data.collectionStatus);
+
+            if (collectionStatus === 0 || collectionStatus === 4) {
+              setPaidStatus(false);
+            } else {
+              setPaidStatus(true);
+            }
+          }
+        }
+        setIsLoading(false);
+      })
+      .catch((error) => {
+        console.log(error.response.data.message);
+        setIsLoading(false);
+      });
+  };
+
   return (
     <div>
       {isLoading ? (
@@ -11,7 +76,7 @@ export default function Order() {
           <div>
             <Header
               subHeading={
-                'Please proceed with the payment to view the generated results.'
+                'Please proceed with the payment to view the created content.'
               }
             />
           </div>
