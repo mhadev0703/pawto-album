c15689521f7b4effcece482b9cf03bf4c807dfd4
mhadev0703 <minchulha0703@gmail.com>
2024-11-28 23:58:57 -0500

    Implement Toss payment UI display

--- packages/web/src/pages/Order/Order.jsx
+++ packages/web/src/pages/Order/Order.jsx
@@ -1,12 +1,15 @@
 import { useEffect, useState } from 'react';
 import { useSearchParams } from 'react-router-dom';
 import axios from 'axios';
+import { loadTossPayments } from '@tosspayments/payment-sdk';
+
 import Loading from '../../components/Loading/Loading';
 import Header from '../../components/Header/Header';
 import Table from '../../components/Table/Table';
 
 const REACT_APP_API_ADDRESS = import.meta.env.VITE_API_URL;
 const REACT_APP_ADDRESS = import.meta.env.VITE_APP_URL;
+const VITE_TOSS_CLIENT_KEY = import.meta.env.VITE_TOSS_CLIENT_KEY;
 
 export default function Order() {
 
@@ -31,8 +34,10 @@ export default function Order() {
   const [paidStatus, setPaidStatus] = useState(false);
   const [productInfoData, setProductInfoData] = useState([]);
   const [paymentInfoData, setPaymentInfoData] = useState([]);
-  const [price, setPrice] = useState(2.99);
-
+  //const [price, setPrice] = useState(2.99);
+  const [price, setPrice] = useState(3900);
+  
+  // Check the payment status when the page is loaded
   useEffect(() => {
     if (collectionId !== null) {
       checkPaidStatus();
@@ -61,7 +66,8 @@ export default function Order() {
       },
     ]);
 
-    setPaymentInfoData([{ title: 'TOTAL', content: `$${price}` }]);
+    //setPaymentInfoData([{ title: 'TOTAL', content: `$${price}` }]);
+    setPaymentInfoData([{ title: 'TOTAL', content: `${price}KRW` }]);
   }, [collectionData, price]);
 
   const checkPaidStatus = async () => {
@@ -94,6 +100,31 @@ export default function Order() {
       .catch((error) => {
         console.log(error.response.data.message);
         setIsLoading(false);
+      });
+  };
+
+  const processCheckout = () => {
+    let clientKey = VITE_TOSS_CLIENT_KEY;
+
+    const successUrl = REACT_APP_ADDRESS + '/payment-success';
+    const failUrl = REACT_APP_ADDRESS + '/payment-fail';
+
+    loadTossPayments(clientKey)
+      .then((tossPayments) => {
+        tossPayments.requestPayment('Card', {
+          // Payment information parameters
+          amount: price,
+          orderId: collectionId,
+          orderName: `Pawto Album`,
+          successUrl: successUrl,
+          failUrl: failUrl,
+        });
+      })
+      .catch((error) => {
+        console.log(error);
+        if (error.code === 'USER_CANCEL') {
+          alert('Your payment has been cancelled.');
+        }
       });
   };
 
@@ -132,7 +163,13 @@ export default function Order() {
                   style={{ margin: '40px 0' }}
                   className='center-button'
                 >
-                  <button>CHECKOUT</button>
+                  <button
+                    onClick={() => {
+                      processCheckout();
+                    }}
+                  >
+                    CHECKOUT
+                  </button>
                 </div>
               </div>
             )}
