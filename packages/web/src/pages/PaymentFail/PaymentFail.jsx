c65b34032c1f45ec41331d9c35a646da0f1d462c
mhadev0703 <minchulha0703@gmail.com>
2024-11-30 21:17:44 -0500

    Implement payment validation components

--- packages/web/src/pages/PaymentFail/PaymentFail.jsx
+++ packages/web/src/pages/PaymentFail/PaymentFail.jsx
@@ -1,3 +1,56 @@
-export default function PaymentFail() { 
-  return <div>PaymentFail</div>; 
+import { useState, useEffect } from 'react';
+import { useSearchParams } from 'react-router-dom';
+
+import Header from '../../components/Header/Header';
+import fail from '../../assets/images/fail.png';
+
+export default function PaymentFail() {
+  const [searchParams, setSearchParams] = useSearchParams();
+  const code = searchParams.get('code');  // Get the error code from the URL
+
+  const [message, setMessage] = useState('The payment was not completed');  // Set the default message
+
+  // Set the error message based on the error code
+  useEffect(() => {
+    if (code !== null) {
+      switch (code) {
+        case 'PAYMENT_CANCELED_BY_USER':
+          setMessage('The payment was canceled by the user.');
+          break;
+        case 'PAYMENT_AUTHORIZATION_FAILED':
+          setMessage('The payment authorization failed.');
+          break;
+        case 'PAYMENT_DECLINED_BY_CARD_COMPANY':
+          setMessage('The payment was declined by the card company.');
+          break;
+        default:
+          setMessage('The payment failed for an unknown reason.');
+          break;
+      }
+    }
+  }, [code]);
+
+  const subHeading = () => {
+    return (
+      <>
+        {message}
+        <br />
+        Please try again.
+      </>
+    );
+  };
+
+  return (
+    <div className='App'>
+      <Header subHeading={subHeading()} />
+      <div style={{ minHeight: '326px' }} className='container'>
+        <div style={{ marginBottom: '25px' }}>
+          <img src={fail} alt='payment fail' />
+        </div>
+        <div className='payment-notification-text'>
+          Something went wrong.
+        </div>
+      </div>
+    </div>
+  );
 }
--- packages/web/src/pages/PaymentSuccess/PaymentSuccess.jsx
+++ packages/web/src/pages/PaymentSuccess/PaymentSuccess.jsx
@@ -1,3 +1,94 @@
-export default function PaymentSuccess() { 
-  return <div>PaymentSuccess</div>; 
+import { useState, useEffect } from 'react';
+import { useSearchParams } from 'react-router-dom';
+import axios from 'axios';
+
+import Header from '../../components/Header/Header';
+import Loading from '../../components/Loading/Loading';
+import success from '../../assets/images/success.png';
+
+const REACT_APP_API_ADDRESS = import.meta.env.VITE_API_URL;
+const REACT_APP_ADDRESS = import.meta.env.VITE_APP_URL;
+const VITE_TOSS_CLIENT_KEY = import.meta.env.VITE_TOSS_CLIENT_KEY;
+
+export default function PaymentSuccess() {
+  const [searchParams, setSearchParams] = useSearchParams();
+  // Get the paymentKey, orderId, and amount from the URL
+  const paramPaymentKey = searchParams.get('paymentKey');
+  const paramOrderId = searchParams.get('orderId');
+  const paramAmount = searchParams.get('amount');
+
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    if (
+      paramPaymentKey !== null &&
+      paramOrderId !== null &&
+      paramAmount !== null
+    ) {
+      // Send the payment information to the server
+      let url = `${REACT_APP_API_ADDRESS}/payment`;
+      let data = {
+        collectionId: paramOrderId,
+        paymentKey: paramPaymentKey,
+        price: paramAmount,
+      };
+      let config = { 'Content-Type': 'application/json' };
+
+      setIsLoading(true); 
+      
+      axios
+        .post(url, data, config)
+        .then((res) => {
+          console.log(res.data);
+          setIsLoading(false);
+          let url = `${REACT_APP_API_ADDRESS}/execBanana`;
+          let data = {
+            collectionId: paramOrderId,
+          };
+
+          axios
+            .post(url, data, config)
+            .then((res) => {
+              console.log(res.data);
+            })
+            .catch((error) => {
+              console.log(error);
+            });
+        })
+        .catch((error) => {
+          console.log(error.response.data.message);
+          setIsLoading(false);
+        });
+    }
+  }, [paramOrderId, paramPaymentKey, paramAmount]);
+
+  const subHeading = () => {
+    return (
+      <>
+        The payment has been completed.
+        <br />
+        Please check the link sent to your email to track the progress.
+      </>
+    );
+  };
+
+  return (
+    <div>
+      {isLoading ? (
+        <Loading />
+      ) : (
+        <div className='App'>
+          <Header subHeading={subHeading()} />
+          <div style={{ minHeight: '326px' }} className='container'>
+            <div style={{ marginBottom: '25px' }}>
+              <img src={success} alt='payment successful' />
+            </div>
+            <div className='payment-notification-text'>
+              Payment Successful!
+            </div>
+          </div>
+        </div>
+      )}
+    </div>
+  );
 }
