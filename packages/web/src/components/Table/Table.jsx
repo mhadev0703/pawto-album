893c6fcc28b02988337cf91c55cb86afacca5cad
mhadev0703 <minchulha0703@gmail.com>
2024-12-01 23:00:02 -0500

    Add content renderer component

--- packages/web/src/components/Table/Table.jsx
+++ packages/web/src/components/Table/Table.jsx
@@ -1,9 +1,45 @@
 import PropTypes from 'prop-types';
 import './Table.css';
+import ProgressBar from '../ProgressBar/ProgressBar';
 
 export default function Table(props) {
   const tableTitle = props.tableTitle;
   const tableContent = props.tableContent;
+
+  // Render the content based on the type
+  const contentRenderer = (content) => {
+    switch (content.type) {
+      case 'receipt':
+        return (
+          <a
+            className='link-button'
+            target='_blank'
+            href={content.content}
+          >
+            Receipt
+          </a>
+        );
+      case 'progress':
+        return (
+          <ProgressBar
+            backgroundColor={'#F2D7C0'}
+            progressValue={content.content}
+          />
+        );
+      case 'result':
+        return (
+          <a
+            className='link-button'
+            target='_blank'
+            href={content.content}
+          >
+            View Generated Images
+          </a>
+        );
+      default:
+        return content.content;
+    }
+  };
 
   return (
     <div className='table-container'>
@@ -14,6 +50,7 @@ export default function Table(props) {
             <div className='table-row' key={index}>
               <div className='table-col'>{content.title}</div>
               <div className='table-col'>{content.content}</div>
+              {contentRenderer(content)}
             </div>
           );
         })}
