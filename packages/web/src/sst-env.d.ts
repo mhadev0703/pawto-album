48f134eb7dc1d03e28e04ea77325d5fca6805504
mhadev0703 <minchulha0703@gmail.com>
2025-10-05 12:06:51 -0400

    chore: add TOSS test client key type

--- packages/web/src/sst-env.d.ts
+++ packages/web/src/sst-env.d.ts
@@ -2,6 +2,7 @@
 interface ImportMetaEnv {
   readonly VITE_API_URL: string
   readonly VITE_APP_URL: string
+  readonly VITE_TOSS_CLIENT_KEY: string
 }
 interface ImportMeta {
   readonly env: ImportMetaEnv
